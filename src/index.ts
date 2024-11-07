6e37897ca877e8a396fd37e75947ab5b7dd8bfe4
RileyBlackk <144529788+RileyBlackk@users.noreply.github.com>
2024-11-06 23:26:37 -0800

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+//create an API
+
